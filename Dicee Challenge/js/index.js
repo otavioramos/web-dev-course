66d877512de57392fb91a2a66f1c4a2a349fca36
Otavio Ramos <otavio.santana@portaldedocumentos.com.br>
2021-10-16 15:06:27 -0300

    My first solution

---
+++ Dicee Challenge/js/index.js
@@ -0,0 +1,37 @@
+function rollTheDice() {
+    var randomNumber1 = getRandomDice("img1");
+    var randomNumber2 = getRandomDice("img2");
+
+    changeMessage(randomNumber1, randomNumber2);
+}
+
+function getRandomDice(className) {
+    var diceImagePath = "images/dice";
+
+    var randomNumber = Math.floor(Math.random() * 6) + 1;
+    
+    className = "." + className;
+    diceImagePath = diceImagePath + randomNumber + ".png";    
+
+    diceElement = document.querySelector(className);
+
+    diceElement.setAttribute("src", diceImagePath);
+
+    return randomNumber;
+}
+
+function changeMessage(diceNumber1, diceNumber2) {
+    var messageElement = document.querySelector(".container h1");
+
+    var happyIcon = '<i class="fas fa-smile-beam happy-icon"></i>';
+
+    if (diceNumber1 === diceNumber2) {
+        messageElement.innerHTML = "Draw";
+    } else if (diceNumber1 > diceNumber2) {
+        messageElement.innerHTML = happyIcon + " Player 1 Wins!";
+    } else {
+        messageElement.innerHTML = "Player 2 Wins! " + happyIcon;
+    }
+}
+
+rollTheDice();
