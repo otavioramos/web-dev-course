26bed082015a4f8b5cb461abf5713353ce2dff19
Otavio Ramos <otavio.santana@portaldedocumentos.com.br>
2021-10-21 11:19:29 -0300

    Adicionando som e imagem aos botões.

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,5 +1,8 @@
 var drumButtons = document.querySelectorAll(".drum")
 
 drumButtons.forEach(button => {
-    button.addEventListener("click", () => alert("I got clicked!"))
+    button.addEventListener("click", function () {
+        var audio = new Audio("sounds/tom-1.mp3")
+        audio.play()
+    })
 });
