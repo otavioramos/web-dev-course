10730ab42ff8091fdb7527aa81a7efe962d42b42
Otavio Ramos <otavio.santana@portaldedocumentos.com.br>
2021-10-26 17:18:14 -0300

    Testando evento keydown com jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,10 @@
-$("button").click(function () {
-    $("h1").css("color","purple");
-})
+$(document).keydown( (event) => {
+    $("h1").text(event.key);
+});
+
+$("h1").on("click", () => {
+    if ($("h1").css("color") === "rgb(255, 255, 0)")
+        $("h1").css("color","purple");
+    else
+        $("h1").css("color","yellow");
+});
